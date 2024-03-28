846783b871f56706bb4c93a90cc9767b1442c9f6
cnobleshriver <100378265+cnobleshriver@users.noreply.github.com>
2024-03-28 16:49:45 -0400

    task #3

--- src/server.js
+++ src/server.js
@@ -1,6 +1,21 @@
 // TASK #2: Import the required modules
 import http from 'http';
 import url from 'url';
+
+let counter = 0;
+
 // TASK #3: Write a request handler
+function requestHandler(req, res) {
+    const pathname = url.parse(req.url, true).pathname
+
+    if (pathname === '/inc') {
+        counter += 1;
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end("Count: " + counter);
+    } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end("Server is running");
+    }
+}
 // TASK #4: Create the web server
 // Add your implementation below.
