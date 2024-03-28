1e7da56cb7d2463a322f95d21a0586972cb9742c
cnobleshriver <100378265+cnobleshriver@users.noreply.github.com>
2024-03-28 17:01:55 -0400

    task #4

--- src/server.js
+++ src/server.js
@@ -6,12 +6,12 @@ let counter = 0;
 
 // TASK #3: Write a request handler
 function requestHandler(req, res) {
-    const pathname = url.parse(req.url, true).pathname
+    const pathname = url.parse(req.url, true).pathname;
 
     if (pathname === '/inc') {
         counter += 1;
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end("Count: " + counter);
+        res.end(`<b>Count: ${counter}</b>`)
     } else {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end("Server is running");
@@ -19,3 +19,8 @@ function requestHandler(req, res) {
 }
 // TASK #4: Create the web server
 // Add your implementation below.
+
+const server = http.createServer(requestHandler);
+server.listen(3000, () => {
+    console.log(`Server started on port 3000`);
+  });
